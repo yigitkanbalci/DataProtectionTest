029173041a39bdf5b7cac207359b8dcd99f439d7
yigitkanbalci <yigitkan.balci@gmail.com>
2021-05-30 19:44:31 +0300

    controller side CORS enable

--- DataProtectionAPI/Controllers/WeatherForecastController.cs
+++ DataProtectionAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataProtectionAPI.Controllers
 {
+    [EnableCors("AllowSites")]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
